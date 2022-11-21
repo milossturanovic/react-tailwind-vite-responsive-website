ccf961559cf65951eaf987ac398eb1178bd4a7b8
unknown <sturanovic.ms@gmail.com>
2022-11-21 01:25:36 +0100

    added CardDeal component

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import{apple, bill, google} from '../assets';
+import styles, {layout} from '../style';
 
 function Billing() {
   return (
-    <div>Billing</div>
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]' />
+     
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"/>
+      
+      </div>
+
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Easily control your 
+        <br className="sm:block hidden"/> billing & invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea possimus vero eligendi. Placeat quas doloremque praesentium, libero itaque id quos.
+        </p>
+
+        <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+          <img src={apple} alt="apple-store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' /> 
+           <img src={google} alt="apple-store" className='w-[128px] h-[42px] object-contain cursor-pointer' />  
+                  </div>
+      </div>
+
+    </section>
   )
 }
 
--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {card} from '../assets';
+import styles, {layout} from '../style';
+import Button from './Button';
+
 
 function CardDeal() {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/>
+        in a few steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, provident eaque beatae velit nostrum sit iure consectetur. Praesentium aliquam, corrupti id sapiente omnis iusto doloremque voluptatum laboriosam provident velit ipsam!
+
+        </p>
+        <Button styles="mt-10 "></Button>
+      </div>
+
+        <div className={layout.sectionImg}>
+          <img src={card} alt="card" className='w-[100%] h-[100%]' />
+        </div>
+ 
+
+    </section>
   )
 }
 
